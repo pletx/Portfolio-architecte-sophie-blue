87f17d2df6dd973410edd219b3e9653b7246f43e
Eddy <eddytougon@gmail.com>
2023-01-31 21:24:29 +0100

    feat: recupération données API

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,19 +1,23 @@
 const zone_images = document.getElementById("gallery");
-let figure=document.createElement("figure")
+let figure = document.createElement("figure")
 let img = document.createElement("img");
-let figcap=document.createElement("figcaption")
+let figcap = document.createElement("figcaption")
 fetch('http://localhost:5678/api/works')
     .then(response => response.json())
-    .then(data => {let data_url =[];
+    .then(data => {
+        let data_url = [];
         for (let url in data) {
-            data_url.push(data[url]["imageUrl"]);} 
-        console.log(data_url); 
+            data_url.push(data[url]["imageUrl"]);
+        }
+        console.log(data_url);
 
-        for(let i=0; i< data_url.length; i++){
+        for (let i = 0; i < data_url.length; i++) {
             img.src = data_url[i];
             figure.appendChild(img)
             figure.appendChild(figcap)
-            zone_images.appendChild(figure);}
-          })
+            zone_images.appendChild(figure);
+        }
+    })
+    .catch(error => console.error(error))
 
 
