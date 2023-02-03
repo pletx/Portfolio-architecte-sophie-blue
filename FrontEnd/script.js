e5b86b52830299472239c2efdb993e8b865de2e3
Eddy <eddytougon@gmail.com>
2023-02-03 16:07:15 +0100

    feat:affichage dynamique img +titre

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,23 +1,32 @@
-const zone_images = document.getElementById("gallery");
-let figure = document.createElement("figure")
-let img = document.createElement("img");
-let figcap = document.createElement("figcaption")
+const zone_images = document.querySelector(".gallery");
 fetch('http://localhost:5678/api/works')
     .then(response => response.json())
     .then(data => {
-        let data_url = [];
-        for (let url in data) {
-            data_url.push(data[url]["imageUrl"]);
-        }
-        console.log(data_url);
-
-        for (let i = 0; i < data_url.length; i++) {
-            img.src = data_url[i];
-            figure.appendChild(img)
-            figure.appendChild(figcap)
-            zone_images.appendChild(figure);
-        }
+    affihage_img(data)
+    
+    console.log(data);
     })
     .catch(error => console.error(error))
 
-
+    function recuperation(data,nom_clé) {
+        let tableau = [];
+        for (let donnée in data) {
+            tableau.push(data[donnée][nom_clé]);
+        }
+        return tableau
+      }
+    function affihage_img(data){
+        let data_url=recuperation(data,"imageUrl")
+        for (let i = 0; i < data_url.length; i++) {
+        let img = document.createElement("img");
+        let figure = document.createElement("figure")
+        let figcap = document.createElement("figcaption")
+        img.src = data_url[i];
+        img.crossOrigin="anonymous"
+        figure.appendChild(img)
+        figure.appendChild(figcap)
+        zone_images.appendChild(figure);
+        figcap.innerHTML=recuperation(data,"title")[i]
+       
+    }
+    }
